997167e7932fa25025161c5bafbbab8fe6efb604
Arifulislam332 <ayatullaharif332@gmail.com>
2024-10-28 12:36:51 +0600

    header added home page

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import { assets } from "@/assets/assets_frontend/assets";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="container px-10 sm:px-0 mx-auto mt-5">
+      <div className="bg-purpule_blue rounded-lg flex flex-col md:flex-row flex-wrap px-6 md:px-10 lg:px-20">
+        {/* -------- LEFT SIDE-------- */}
+        <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
+            Book Appoinment With Trusted Doctors
+          </h1>
+          <div className="flex items-center gap-3 md:flex-row text-white text-sm font-light">
+            <img className="w-28" src={assets.group_profiles} alt="" />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing Fuga nam error
+              ex voluptates aperiam consequatur ratione
+            </p>
+          </div>
+          <Link>
+            <Button className="rounded-full bg-white text-black font-medium hover:bg-gray-200">
+              Book Appoinment{" "}
+              <img className="w-3" src={assets.arrow_icon} alt="" />{" "}
+            </Button>
+          </Link>
+        </div>
+
+        {/* -------- RIGHT SIDE-------- */}
+        <div className="md:w-1/2 relative">
+          <img
+            className="w-full md:absolute bottom-0 h-auto rounded-lg"
+            src={assets.header_img}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import Hero from "../components/Hero";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
     <div>
-      <Hero />
+      <Header />
     </div>
   );
 };
